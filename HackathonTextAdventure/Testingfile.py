5dd95d42f2c3f6562350338bae47773458bc0ea8
JPAshour <jpashour@tamu.edu>
2019-09-07 10:38:31 -0500

    Init

---
+++ HackathonTextAdventure/Testingfile.py
@@ -0,0 +1,7 @@
+userInput = input("Welcome to our text adventure! Are you ready to start? Y/N ")
+
+if userInput in ['Y', 'y']:
+    print("Awesome!")
+elif userInput in ['N', 'n']:
+    print("Maybe Later")
+
